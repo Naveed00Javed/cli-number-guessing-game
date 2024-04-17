63f518e462e3e4848724220b7f4836b61a27604d
Naveed Javed <163446043+Naveed00Javed@users.noreply.github.com>
2024-04-17 23:04:33 +0500

    cli-number-guessing-game

---
+++ index.js
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+console.log("\n\wellcome to codewithNaveed - CLI Number Guessing Game\n");
+const randomNumber = Math.floor(Math.random() * 5 + 1);
+const answer = await inquirer.prompt([
+    {
+        name: "userGuesedNumber",
+        type: "number",
+        message: "Enter your Geuess Number(Nmber limit from 1 to 5):",
+    },
+]);
+if (answer.userGuesedNumber === randomNumber) {
+    console.log("Congratulation ! You Guess a correct Number.");
+}
+else {
+    console.log("Sorry, You Guess a wrong Number");
+}
